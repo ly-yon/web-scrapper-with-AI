a2b8cdcd47cbba1adb3dd706c038b96ef46c1a42
ly-yon <132755281+ly-yon@users.noreply.github.com>
2024-08-11 14:30:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import main from "./main-multi-language.js";
-main("https://www.luluhypermarket.com/en-bh").then((res) => {
-  // console.log(res);
-  console.log(`The results :-
+const args = process.argv.slice(2);
+const url = args[0];
+if (url)
+  main(url).then((res) => {
+    // console.log(res);
+    console.log(`The results :-
     Cart URL:- ${res.cart.url}
     Cart Validity:- ${res.cart.status}
     -------------------------------------------
@@ -18,4 +21,4 @@ main("https://www.luluhypermarket.com/en-bh").then((res) => {
     This Result's are Generated via pre-Trained Data!
     Consider 3-5% Error in the Results, However the 
     Results are very good!!`);
-});
+  });
