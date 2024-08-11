b9ec6bfa1b85216555ac49d80ee3a5be4e58fbb0
ly-yon <132755281+ly-yon@users.noreply.github.com>
2024-08-11 14:35:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,5 @@ const url = args[0];
 if (url)
   main(url).then((res) => {
     // console.log(res);
-    console.log(`The results :-
-    Cart URL:- ${res.cart.url}
-    Cart Validity:- ${res.cart.status}
-    -------------------------------------------
-    Refund URL:- ${res.refund.url}
-    Refund Validity:- ${res.refund.status}
-    -------------------------------------------
-    Privacy URL:- ${res.policy.url}
-    Privacy Validity:- ${res.policy.status}
-    -------------------------------------------
-    Terms URL:- ${res.term.url}
-    Terms Validity:- ${res.term.status}
-    -------------------------------------------
-    DISCLAIMER:-
-    This Result's are Generated via pre-Trained Data!
-    Consider 3-5% Error in the Results, However the 
-    Results are very good!!`);
+    console.log(res);
   });
