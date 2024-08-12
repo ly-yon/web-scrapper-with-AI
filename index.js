449f96aef73e216ac704f31554e205a189e1d1f8
ly-yon <132755281+ly-yon@users.noreply.github.com>
2024-08-12 10:15:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import main from "./main-multi-language.js";
-const args = process.argv.slice(2); // Remove Node.js and script name
-const url = args[0]; // Get the URL (expected as the first argument)
+const url = process.argv[2];
+function checkDomain(urlString) {
+  try {
+    const url = new URL(urlString);
+    return true;
+  } catch (error) {
+    return false; // Or handle the error as needed
+  }
+}
 async function run(url) {
-  if (url) {
+  if (checkDomain(url)) {
     const result = await main(url);
     console.log(JSON.stringify(result));
   } else console.log(JSON.stringify({ url: "Not Valid" }));
